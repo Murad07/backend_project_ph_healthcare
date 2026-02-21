a1105e21226eb823a437b71970bc30b0e36935c5
Murad07 <murad.pi22@gmail.com>
2026-02-21 10:21:48 +0600

    feat: Module 42 prescription

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,9 @@ import { AppointmentRoutes } from "../module/appointment/appointment.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { DoctorRoutes } from "../module/doctor/doctor.route";
 import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
+import { PatientRoutes } from "../module/patient/patient.route";
+import { PrescriptionRoutes } from "../module/prescription/prescription.route";
+import { ReviewRoutes } from "../module/review/review.route";
 import { scheduleRoutes } from "../module/schedule/schedule.route";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { UserRoutes } from "../module/user/user.route";
@@ -13,11 +16,14 @@ const router = Router();
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRoutes)
 router.use("/users", UserRoutes)
+router.use("/patients", PatientRoutes)
 router.use("/doctors", DoctorRoutes)
 router.use("/admins", AdminRoutes)
 router.use("/schedules", scheduleRoutes)
 router.use("/doctor-schedules", DoctorScheduleRoutes)
 router.use("/appointments", AppointmentRoutes)
+router.use("/prescriptions", PrescriptionRoutes)
+router.use("/reviews", ReviewRoutes)
 
 
 export const IndexRoutes = router;
